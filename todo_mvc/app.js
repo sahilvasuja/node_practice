af08a6a75fa9a54a38553eda44a2406ae4597bf2
Sahil Vasuja <sahilvasuja@Sahil-Vasujas-MacBook-Pro.local>
2023-01-06 16:25:57 +0530

    delete function completed

--- todo_mvc/app.js
+++ todo_mvc/app.js
@@ -23,7 +23,12 @@ app.post("/add_todo",(req,res)=>{
         if (err) throw err;
         console.log("19",result);
         let jsonData = JSON.parse(data);
-        jsonData.push(result);
+        const todo={
+            task: result,
+            iscompleted: false,
+
+        }
+        jsonData.push(todo);
     fs.writeFile('database.json',JSON.stringify(jsonData), (err) => {
         if (err) throw err;
        
@@ -31,6 +36,31 @@ app.post("/add_todo",(req,res)=>{
       });
      });
 })
+app.get("/delete/:id", async(req,res)=>{
+    
+        const id = req.params.id;
+      
+      console.log(id,"37")
+    // // Retrieve the to-do list from local storage
+    fs.readFile('database.json', (err, data) => {
+        if (err) throw err;
+      
+        const jsonData = JSON.parse(data);
+        
+        jsonData.splice(id,1);
+        console.log(jsonData);
+        fs.writeFile('database.json', JSON.stringify(jsonData), (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+          });
+     });
+    
+        console.log("Deleted Todo Successfully!");
+        res.redirect("/");
+      
+     
+ })
+
 app.listen(8000,()=>{
     console.log("sever connected")
 })
