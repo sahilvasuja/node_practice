1459686af985ed6d2059a667c0488f8d93cae35c
Sahil Vasuja <sahilvasuja@Sahil-Vasujas-MacBook-Pro.local>
2023-01-11 19:17:53 +0530

    todo completed

--- mvctodo_js/todos.js
+++ mvctodo_js/todos.js
@@ -9,23 +9,36 @@ class Mytask{
         this.todoItems=[];
     }
     icon(){
-        console.log(icon)
+       
         let completeditem=0;
+        let notcompleted=0;
         for(let i in this.todoItems){
             if(this.todoItems[i].isCompleted){
+                console.log("1")
                 completeditem++;
-            }
+            }    
         }
-        if(completeditem==count){
-            for(let i in this.todoItems){
-                this.todoItems[i].isCompleted=false;
-            }
-        }
-        else{
+        if(completeditem!=this.todoItems.length){
+            console.log("first inner")
             for(let i in this.todoItems){
                 this.todoItems[i].isCompleted=true;
             }
+           
         }
+        
+        else{
+                for(let i in this.todoItems){
+                this.todoItems[i].isCompleted=false;
+            }
+        }
+
+        // else{
+        //     console.log("2")
+        //     for(let i in this.todoItems){
+        //         this.todoItems[i].isCompleted=true;
+        //     }
+         
+        // }
         this.display()
     }
     count(){
@@ -38,12 +51,21 @@ class Mytask{
             }
             
        }
-       console.log(count,"count")
+    //    if(count>=1){
+    //     let footer=document.getElementById("foot")
+    //     footer.style.visibility="visible"
+    //    }
+    //    else{
+    //     let footer=document.getElementById("foot")
+    //     footer.style.visibility="hidden"
+    //    }
+       
+       document.getElementById("count").innerHTML=` ${count} items left`;
     }
     add(items,e){
         e.preventDefault();
         this.todoItems.push(items)
-        // console.log(items,"add")
+       
         this.display()
     }
     remove(index){
@@ -71,9 +93,9 @@ class Mytask{
         for(let i in this.todoItems){
             if(this.todoItems[i].isCompleted==true){
                 html+=`<div class="inneradd">
-                    <input type="checkbox" id="checkbox" onclick="mytask.checkbox(${i})" checked>
-                    <p> ${this.todoItems[i].task} </p>
-                    <p id="cross">X</p>
+                    <input type="checkbox" id="checkbox"  onclick="mytask.checkbox(${i})" checked>
+                    <p class="checkline"> ${this.todoItems[i].task} </p>
+                    <p id="cross" onclick="mytask.remove(${i})">X</p>
                     </div>
                 `
             }
@@ -96,6 +118,7 @@ class Mytask{
         mytask.display();
     }
     active(){
+        let additem=document.getElementById("addtodo");
         let html=""
         for(let i in this.todoItems){
             if(this.todoItems[i].isCompleted==false){
@@ -107,44 +130,49 @@ class Mytask{
                 `
             }
         }
-        this.display()
+        additem.innerHTML=html
+        // this.display()
         console.log(this.todoItems,"act")
     }
     completed(){
+
+        let additem=document.getElementById("addtodo");
         let html=""
         for(let i in this.todoItems){
             if(this.todoItems[i].isCompleted==true){
                 html+=` <div class="inneradd"> 
-                <input type="checkbox" id="checkbox" onclick="mytask.checkbox()" checked>
-                <p> ${this.todoItems[i].task} </p>
+                <input type="checkbox" class="checkline" id="checkbox" onclick="mytask.checkbox()" checked>
+                <p class="checkline"> ${this.todoItems[i].task} </p>
                 <p id="cross" onclick="mytask.remove(${i})">X</p>
                 </div>
                 `
             }
         }
-        this.display()
+        additem.innerHTML=html
+        // this.display()
         console.log(this.todoItems,"comp")
     }
     clear(){
-        console.log("first, todoItems")
-        for(let i in this.todoItems){
-            if(this.todoItems[i].isCompleted==true){
-                this.todoItems.splice(i,1)
-            }
-        }
-        console.log(this.todoItems,"clear")
+        console.log("first", this.todoItems)
+        this.todoItems=this.todoItems.filter(function(index){
+            
+               return !index.isCompleted
+            
+        })
+        console.log(this.todoItems);
+        // console.log(index,"clear")
          this.display()
     }
 
 }
 let text=document.getElementById("text");
-// let itemcount=document.getElementById("count");
-// itemcount.innerText("56")
 let mytask=new Mytask();
 
 text.addEventListener("keydown",(e)=>{
     
     if(e.key === 'Enter' && text.value!=""){
+        let footer=document.getElementById("foot")
+        footer.style.visibility="visible"
         let task=e.target.value;
         let newTask=new Todo(task,false)
         mytask.add(newTask,e);
@@ -158,14 +186,25 @@ icon.addEventListener("click",function(){
 })
 let all=document.getElementById("all");
 all.addEventListener("click", function(){
+    active.style.border="none"
+    completed.style.border="none"
+    all.style.border="1px solid rgb(235,216,215)"
     mytask.all();
 })
 let active=document.getElementById("active");
 active.addEventListener("click",function(){
+    all.style.border="none"
+    completed.style.border="none"
+    active.style.border="1px solid rgb(235,216,215)"
+    active.style.padding="3px"
     mytask.active()
 })
 let completed=document.getElementById("completed");
 completed.addEventListener("click",function(){
+    all.style.border="none"
+    active.style.border="none"
+    completed.style.border="1px solid rgb(235,216,215)"
+    completed.style.padding="3px"
     mytask.completed()
 })
 let clear=document.getElementById("clear");
